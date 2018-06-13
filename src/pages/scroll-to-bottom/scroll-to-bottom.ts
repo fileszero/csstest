cec450a8f3442ae8a324b80404be8ba60fcab077
FILES=0 <files.eq.zero@gmail.com>
2018-06-13 11:40:06 +0900

    set timer event and show contents

--- src/pages/scroll-to-bottom/scroll-to-bottom.ts
+++ src/pages/scroll-to-bottom/scroll-to-bottom.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, DateTime } from 'ionic-angular';
 
 /**
  * Generated class for the ScrollToBottomPage page.
@@ -14,12 +14,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'scroll-to-bottom.html',
 })
 export class ScrollToBottomPage {
-
+  timeline: string[] = [];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ScrollToBottomPage');
+    this.timeline.push("first line!");
+    this.setTimer();
+  }
+
+  setTimer() {
+    setTimeout(() => {
+      const now = Date.now();
+      this.timeline.push(now.toString());
+      if (this.navCtrl.last().instance === this) {
+        this.setTimer();
+      }
+    }, 1000);
   }
 
 }
