140cdd813b29c54a41a18a9e22293b04a7877e08
FILES=0 <files.eq.zero@gmail.com>
2018-07-11 12:56:11 +0900

    ionic g component ProgressBar

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KanjiDrawComponent } from './kanji-draw/kanji-draw';
 import { ProgressButtonComponent } from './progress-button/progress-button';
+import { ProgressBarComponent } from './progress-bar/progress-bar';
 @NgModule({
   declarations: [KanjiDrawComponent,
-    ProgressButtonComponent],
+    ProgressButtonComponent,
+    ProgressBarComponent],
   imports: [CommonModule],
   exports: [KanjiDrawComponent,
-    ProgressButtonComponent]
+    ProgressButtonComponent,
+    ProgressBarComponent]
 })
 export class ComponentsModule { }
