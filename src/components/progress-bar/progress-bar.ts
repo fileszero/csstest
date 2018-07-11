49f2461873d2a62524c811bb815ef4974808e375
FILES=0 <files.eq.zero@gmail.com>
2018-07-11 14:11:07 +0900

    implement progress-bar

--- src/components/progress-bar/progress-bar.ts
+++ src/components/progress-bar/progress-bar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 /**
  * Generated class for the ProgressBarComponent component.
@@ -12,11 +12,83 @@ import { Component } from '@angular/core';
 })
 export class ProgressBarComponent {
 
-  text: string;
-
   constructor() {
     console.log('Hello ProgressBarComponent Component');
-    this.text = 'Hello World';
   }
+  private _progress: number = 0;
+  @Input()
+  set progress(p: number) {
+    this._progress = p;
+  }
+  get progress(): number {
+    if (this._progress < 0) return 0;
+    if (this._progress > 100) return 100;
+    return this._progress;
+  }
+  private _showNumber: boolean = false;
+  @Input()
+  set showNumber(val: boolean) {
+    this._showNumber = val;
+  }
+  get showNumber(): boolean {
+    return this._showNumber;
+  }
+
+  private _showBorder: boolean = false;
+  @Input()
+  set showBorder(val: boolean) {
+    this._showBorder = val;
+  }
+  get showBorder(): boolean {
+    return this._showBorder;
+  }
+
+  private _height: number = 1;
+  @Input()
+  set height(val: number) {
+    this._height = val;
+  }
+  get height(): number {
+    return this._height;
+  }
+
+  private _color: string = "#488aff";
+  @Input()
+  set color(val: string) {
+    this._color = val;
+  }
+  get color(): string {
+    return this._color;
+  }
+
+  outerStyle() {
+    const s = {
+      "width": "96%",
+      // "padding": "1px",
+      "text-align": "center",
+      "background-color": "#f4f4f4",
+      "color": "#fff"
+    }
+    if (this.showBorder) {
+      s["border"] = "1px solid #dcdcdc";
+    }
+    return s;
+  }
+  innerStyle() {
+    const s = {
+      "height": this.height + "px",
+      "width": this.progress + "%",
+      "padding": "0px",
+      "white-space": "nowrap",
+      "overflow": "hidden",
+      "background-color": this.color,
+      "font-size": this.height + "px",
+    }
+    if (this.height > 3) {
+      s["border-radius"] = this.height + "px";
+    }
+    return s;
+  }
+
 
 }
