51e808de391642b35ba4e1fd7f6820ca0f8cc3a4
FILES=0 <files.eq.zero@gmail.com>
2018-07-11 10:43:24 +0900

    link progress page and component

--- src/pages/progress-button/progress-button.module.ts
+++ src/pages/progress-button/progress-button.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ProgressButtonPage } from './progress-button';
-
+import { ComponentsModule } from '../../components/components.module';
 @NgModule({
   declarations: [
     ProgressButtonPage,
   ],
   imports: [
     IonicPageModule.forChild(ProgressButtonPage),
+    ComponentsModule,
   ],
 })
-export class ProgressButtonPageModule {}
+export class ProgressButtonPageModule { }
