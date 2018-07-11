3a2e694620f165b08b83486f862a396ba06f74d8
FILES=0 <files.eq.zero@gmail.com>
2018-07-11 10:15:58 +0900

    ionic g component ProgressButton

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KanjiDrawComponent } from './kanji-draw/kanji-draw';
+import { ProgressButtonComponent } from './progress-button/progress-button';
 @NgModule({
-  declarations: [KanjiDrawComponent],
+  declarations: [KanjiDrawComponent,
+    ProgressButtonComponent],
   imports: [CommonModule],
-  exports: [KanjiDrawComponent]
+  exports: [KanjiDrawComponent,
+    ProgressButtonComponent]
 })
 export class ComponentsModule { }
